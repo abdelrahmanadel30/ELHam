1d967adc0cd22e899584cb80b3df4fb412f7d13a
abdelrahman adel <38409101+abdelrahmanadel30@users.noreply.github.com>
2018-04-16 03:11:06 +0200

    ElHam

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+
+
+
+        // Tap
+        // $(".nav .item ul li").on("mouseenter", function() {
+        //         $(this).addClass("active").siblings().removeClass("active")
+        // });
+        //
+
+});
